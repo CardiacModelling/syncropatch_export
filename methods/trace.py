4b7f7400944653f21345f63e1eecb7d902d56e25
Frankie Patten-Elliot <frankie.patten-elliott@nottingham.ac.uk>
2023-11-27 10:27:23 +1100

    add comments

--- methods/trace.py
+++ methods/trace.py
@@ -3,11 +3,25 @@ import numpy as np
 import string
 
 class Trace:
+    '''
+    Defines a Trace object from the output of a Nanion experiment.
+    Required as input:
+    filepath - path pointing to folder containing .json and .dat files (str)
+    json_file - specific filename of json file (str)
+    '''
     def __init__(self, filepath, json_file):
+        # store file paths
         self.filepath = filepath
-        self.json_file = json_file
+        if ".json" in json_file:
+            self.json_file = json_file
+        else:
+            self.json_file = json_file + ".json"
+        
+        # load json file
         with open(self.filepath + self.json_file) as f:
             self.meta = json.load(f)
+        
+        # store necessary header info
         TraceHeader = self.meta['TraceHeader']
         try:
             self.TimeScaling = TraceHeader['TimeScaling']
@@ -28,12 +42,23 @@ class Trace:
         self.FileList.sort()
 
     def voltage(self):
+        '''
+        Returns the voltage stimulus from Nanion .json file
+        '''
         return self.TimeScaling['Stimulus']
         
     def times(self):
+        '''
+        Returns the time steps from Nanion .json file
+        '''
         return self.TimeScaling['TR_Time']
         
     def all_traces(self, leakcorrect=False):
+        '''
+        Returns all raw current traces from .dat files
+        '''
+        
+        # initialise output
         OUT = {}
         for iCol in self.WELL_ID:
             for ijWell in iCol:
@@ -41,25 +66,38 @@ class Trace:
 
         currentSweep = 0
         for trace_file in self.FileList:
+            # work out the total number of sweeps in the current trace file
             if currentSweep + self.SweepsPerFile > self.NofSweeps:
                 totalSweep = self.NofSweeps - currentSweep
             else:
                 totalSweep = self.SweepsPerFile
             currentSweep += self.SweepsPerFile
             assert(totalSweep > 0)
+            
+            # get trace data
             with open(self.filepath + trace_file, 'r') as f:
                 trace = np.fromfile(f, dtype=np.int16)
+            
+            # convert to numpy array
             trace = np.asarray(trace)
+            
+            # check loaded traces have the expected length
             assert(len(trace) == self.Leakdata * self.NofSamples * self.WP_nRows
                                 * self.nCols * totalSweep)
             idx_i = 0
+            
+            # loop through sweeps and columns 
             for kSweep in range(totalSweep):
                 for i, iCol in enumerate(self.ColsMeasured):
                     if iCol == -1:
-                        continue
+                        continue # -1 not measured (need to doublecheck this)
+
                     idx_f = idx_i + self.Leakdata * self.NofSamples * self.WP_nRows
                     assert(idx_f <= len(trace))
+                    
+                    # convert to double in pA
                     iColTraces = np.array(trace[idx_i:idx_f]) * self.I2DScale[i] * 1e12
+                    
                     iColWells = self.WELL_ID[i]
                     for j, ijWell in enumerate(iColWells):
                         if leakcorrect:
@@ -72,16 +110,22 @@ class Trace:
                             + (leakoffset + 1) * self.NofSamples
                         OUT[ijWell].append(iColTraces[start:end])
                     del(iColTraces)
-                    idx_i = idx_f
+                    idx_i = idx_f # update idx_i
             del(trace)
         return OUT
   
     def get_trace_file(self, sweeps):
+        '''
+        Returns the trace file index of the file for a given set of sweeps
+        '''
         OUT_file_idx = []
         OUT_idx_i = []
         for actSweep in sweeps:
+            # work out trace file idx
             ActFile = int(actSweep/self.SweepsPerFile)
             OUT_file_idx.append(ActFile)
+
+            # work out idx_i
             DataPerWell = self.NofSamples * self.Leakdata
             ColOffset = DataPerWell * self.WP_nRows
             SweepOffset = ColOffset * self.nCols
@@ -90,14 +134,21 @@ class Trace:
         return OUT_file_idx, OUT_idx_i
 
     def get_trace_sweep(self, sweeps, leakcorrect=False):
+        '''
+        Returns a subset of sweeps defined by the input 'sweeps'
+        '''
+        
+        # initialise output
         OUT = {}
         for iCol in self.WELL_ID:
             for ijWell in iCol:
                 OUT[ijWell] = []
         
+        # check `getsweep` input is something sensible
         if len(sweeps) > self.NofSweeps:
             raise ValueError('Required #sweeps > total #sweeps.')
         
+        # convert negative values to positive
         for i, sweep in enumerate(sweeps):
             if sweep < 0:
                 sweeps[i] = self.NofSweeps + sweep
@@ -110,21 +161,24 @@ class Trace:
             else:
                 totalSweep = self.NofSweeps % self.SweepsPerFile
             
+            # get trace data
             trace_file = self.FileList[trace_file_idx]
-            
             with open(self.filepath + trace_file, 'r') as f:
                 trace = np.fromfile(f, dtype=np.int16)
             
+            # convert to numpy array
             trace = np.asarray(trace)
             
+            # check loaded traces have the expected length
             assert(len(trace) == self.Leakdata * self.NofSamples * self.WP_nRows
                                 * self.nCols * totalSweep)
             
             for i, iCol in enumerate(self.ColsMeasured):
                 if iCol == -1:
-                    continue
+                    continue # -1 not measured (need to doublecheck this)
                 idx_f = idx_i + self.Leakdata * self.NofSamples * self.WP_nRows
-                # convert to double in pA!
+                
+                # convert to double in pA
                 iColTraces = trace[idx_i:idx_f] * self.I2DScale[i] * 1e12
                 iColWells = self.WELL_ID[i]
 
@@ -139,6 +193,6 @@ class Trace:
                         + (leakoffset + 1) * self.NofSamples
                     OUT[ijWell].append(iColTraces[start:end])
 
-                idx_i = idx_f
+                idx_i = idx_f # update idx_i
             del(trace)
         return OUT
--- test/test_trace_class.py
+++ test/test_trace_class.py
@@ -20,22 +20,24 @@ def test_get_trace_sweep(trace, sweep):
 
 if __name__ == "__main__":
     filepath = "test/test_files/"
-    json_file = "cisapride_protocol_11.13.12.json"
+    json_file = "cisapride_protocol_11.13.12"
     test_trace = tr(filepath, json_file)
     
-    v = test_voltage(test_trace)
-    ts = test_times(test_trace)   
-    all_trace = test_all_trace(test_trace)
-    some_sweeps = test_get_trace_sweep(test_trace, [0, 1, 2])['A01']
+    ### test trace class attributes
+    v = test_voltage(test_trace) # get voltage
+    ts = test_times(test_trace) # get times  
+    all_trace = test_all_trace(test_trace) # get all traces
+    some_sweeps = test_get_trace_sweep(test_trace, [0, 1, 2])['A01'] # get some sweeps
     print("All test passed")
-
+    
+    ### plot test output
     fig, (ax1, ax2) = plt.subplots(2, 1)
     ax1.set_title("Example Sweeps")
     ax1.plot(ts, np.transpose(some_sweeps), color = 'grey', alpha = 0.5)
-    ax2.set_title("Voltage Protocol")
-    ax2.plot(ts, v)
     ax1.set_ylabel('Current')
     ax1.set_xlabel('Time')
+    ax2.set_title("Voltage Protocol")
+    ax2.plot(ts, v)
     ax2.set_ylabel('Voltage')  
     ax2.set_xlabel('Time')  
     plt.tight_layout()
