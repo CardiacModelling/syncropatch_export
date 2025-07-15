92707e95f78bc837ff5473bec2f0b88d4bb4fe90
Michael Clerx <michael.clerx@nottingham.ac.uk>
2025-07-15 16:45:39 +0100

    Tidied up docs config

--- docs/make.bat
+++ docs/make.bat
@@ -9,7 +9,7 @@ if "%SPHINXBUILD%" == "" (
 )
 set SOURCEDIR=source
 set BUILDDIR=build
-set SPHINXPROJ=Myokit
+set SPHINXPROJ=syncropath_export
 
 if "%1" == "" goto help
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Syncropatch export'
-# copyright = datkit.COPYRIGHT
+# copyright = syncropath_export.COPYRIGHT
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
