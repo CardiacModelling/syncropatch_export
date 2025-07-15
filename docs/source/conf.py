a35da7ab4a87eff7d787b8e5a922ee16f0dae292
Michael Clerx <michael.clerx@nottingham.ac.uk>
2025-07-15 16:46:46 +0100

    Fixed typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Syncropatch export'
-# copyright = syncropath_export.COPYRIGHT
+# copyright = syncropatch_export.COPYRIGHT
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
